9c97a0b128eacd2a59c89a6ddc6a4efa42fc92ff
Ekansh-Gahlot <60690453+Ekansh-Gahlot@users.noreply.github.com>
2026-02-02 15:01:43 +0530

    Changes

--- src/math.ts
+++ src/math.ts
@@ -10,6 +10,10 @@ export function add(a: number, b: number): number {
 export function subtract(a: number, b: number): number {
   if (typeof a !== 'number' || typeof b !== 'number') {
     throw new Error('Both arguments must be numbers');
+
+
+
+    
   }
 
   return a / b;
