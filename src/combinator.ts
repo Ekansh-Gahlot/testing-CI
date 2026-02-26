6dc8796c48dc9086a34469ebbbf87cdcb8567ec3
Ekansh-Gahlot <60690453+Ekansh-Gahlot@users.noreply.github.com>
2026-02-26 16:05:20 +0530

    Changes

--- src/combinator.ts
+++ src/combinator.ts
@@ -1,3 +1,4 @@
+//Renaming function to better suit the context of the project and it is a pure function, not used downstream
 export const combineAndAddOne = (a: number, b: number): number => {
   return a + b + 1;
 };
