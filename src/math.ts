5333b25445668a68e0006c752caab2d45b07337c
Ekansh-Gahlot <60690453+Ekansh-Gahlot@users.noreply.github.com>
2026-02-27 13:31:30 +0530

    Changes to math.ts

--- src/math.ts
+++ src/math.ts
@@ -65,3 +65,15 @@ export const checkNumberIsEvenOrOdd = (a: number): string => {
     return 'Odd';
   }
 }
+
+export const checkNumberIsPrime = (a: number): boolean => {
+  if (a <= 1) {
+    return false;
+  }
+  for (let i = 2; i <= Math.sqrt(a); i++) {
+    if (a % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
